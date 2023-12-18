3b777e3e457fa18106f4c1e909f99f8b678db0b3
Hebert <hebertdev82@gmail.com>
2023-12-18 16:39:06 -0300

    pinterest verify

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
+      <meta name="p:domain_verify" content="7f018f2a7c99c3748a7184a57d07abcb"/>
       <body className={inter.className}>{children}</body>
     </html>
   )
